c7f9f3c5b6038090d808bd79af798136fe44b759
Latish Sehgal <latish.sehgal@gmail.com>
2015-03-25 16:57:11 -0500

    Homework1

--- HelloDojo/HelloDojo/MainActivity.cs
+++ HelloDojo/HelloDojo/MainActivity.cs
@@ -9,29 +9,44 @@ using Android.OS;
 
 namespace HelloDojo
 {
-	[Activity (Label = "HelloDojo", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
-	{
-		int count = 1;
+    [Activity(Label = "HelloDojo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
 
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			var userName = FindViewById<TextView> (Resource.Id.userName);
-			var welcome = FindViewById<TextView> (Resource.Id.welcome);
-			
-			button.Click += delegate {
-				welcome.Text = string.Format ("Welcome, {0}", userName.Text);
-			};
-		}
-	}
+            var button = FindViewById<Button>(Resource.Id.myButton);
+            var userNameField = FindViewById<TextView>(Resource.Id.userName);
+            var passwordField = FindViewById<TextView>(Resource.Id.password);
+            var welcome = FindViewById<TextView>(Resource.Id.welcome);
+
+            var userLogin = new UserLogin();
+
+
+            button.Click += delegate
+            {
+                welcome.Text = userLogin.Login(userNameField.Text, passwordField.Text);
+            };
+        }
+    }
+    class UserLogin
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public string Login(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+            return string.Format("Welcome, {0}. Your Password is {1}.", UserName, Password);
+        }
+    }
 }
 
 
